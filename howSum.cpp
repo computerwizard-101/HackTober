0937f841a7976017baae5324341395ee0fb1a84b
Abhishek Keshri <60318491+abhikeshri10@users.noreply.github.com>
2022-10-12 14:41:38 +0530

    Add files via upload

---
+++ howSum.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> dp(int n , vector<int> &m)
+{
+    
+    
+
+}
+int main()
+{
+    int n = 7;
+    vector<int> m ;
+    m.push_back(3);
+    m.push_back(4);
+    m.push_back(5);
+    m.push_back(7);
+}
